6221bd0880c010743e86a20062c893dccb0c08ae
Z33DD <suniotan@protonmail.com>
2021-05-07 14:51:16 -0300

    Implementa os testes segundo a API

---
+++ fe-s.c.a.m/backend/tests/functional/test_administrador.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+from backend.app.app.main import app
+
+client = TestClient(app)
+
+def test_read_all_administrador():
+    response = client.get("/administrador")
+    assert response.status_code == 200
+
+def test_read_administrador():
+    administrador_id = 1
+    response = client.get(f'/administrador/{administrador_id}')
+    assert response.status_code == 200
