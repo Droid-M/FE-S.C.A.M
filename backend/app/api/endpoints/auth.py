eb9d5c3d1fa1227b04fa0dfcb4ab6065759f72d8
Z33DD <suniotan@protonmail.com>
2021-05-13 21:44:31 -0300

    Adciona o GET /api/auth para validar o jwt

--- backend/app/api/endpoints/auth.py
+++ backend/app/api/endpoints/auth.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header
 from backend.app.schemas.funcionario import Funcionario
 from backend.app.DAO.FuncionarioDAO import FuncionarioDAO
+from typing import Optional
 import bcrypt
 import jwt
 import re
 
-router = APIRouter()
+router = APIRouter(prefix='/api')
+
+SECRET = '123124'
 
 
-@router.post('/')
+@router.post('/auth')
 async def post_auth(func: Funcionario):
     cpf = re.sub(r'\D', '', func.CPF)
     funcionario = FuncionarioDAO.findByPK(cpf)
@@ -20,8 +23,18 @@ async def post_auth(func: Funcionario):
 
         token = jwt.encode(
             payload,
-            'SECRET_KEY',
+            SECRET,
             algorithm='HS256'
         )
         return {'access_token': token}
     return {'msg': f'Esse funcionário não existe, CPF: {cpf}.'}
+
+
+@router.get('/auth')
+async def ping(authentication: Optional[str] = Header(None)):
+    encoded = re.sub('Bearer ', '', authentication)
+    decoded = jwt.decode(encoded, SECRET, algorithms="HS256")
+    cpf = decoded.get('cpf')
+    if cpf and FuncionarioDAO.findByPK(cpf):
+        return True
+    return False
