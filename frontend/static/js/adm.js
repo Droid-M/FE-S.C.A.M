3c91ebc54d6d820bd66a481422e9d2c45c2f1128
Droid-M <62139366+Droid-M@users.noreply.github.com>
2021-05-21 01:06:10 -0300

    Delete frontend directory

--- frontend/static/js/adm.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $("#sidebar-wrapper").addClass("bg-light border-right");
-    $("#sidebar-wrapper").load("/sidebaradm");
-
-    $("#navbar").addClass("navbar navbar-expand-lg navbar-light bg-light border-bottom");
-    $("#navbar").load("/navbar");
-});
