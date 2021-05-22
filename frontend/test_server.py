51feb67dfc0672500092add9cccbd87aa0ddd974
Z33DD <suniotan@protonmail.com>
2021-05-22 09:51:36 -0300

    Mudanças nos templates

--- frontend/test_server.py
+++ frontend/test_server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from starlette.responses import Response
 
 app = FastAPI()
 
@@ -12,5 +13,11 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
+async def index(request: Request):
     return templates.TemplateResponse("child.html", {"request": request})
+
+
+@app.get("/adm", response_class=HTMLResponse)
+async def adm_index(request: Response):
+    with open('templates/adm.html', 'r') as html:
+        return html.read()
