78a01a0521419d691a3f1d291be1bac79f89a9a7
Droid-M <marcosvferreira92@gmail.com>
2021-05-27 16:56:48 -0300

    preparando arq de dump para backup automatizado

--- backend/fescam/db/create_schemas.py
+++ backend/fescam/db/create_schemas.py
@@ -31,5 +31,15 @@ def create_tables(): #<- Adicionar tratamento de exceção depois ****
     execute(commands)
     
 def create_all(): #<- Adicionar tratamento de exceção depois ****
-    create_types()
-    create_tables()
+    commands = ''
+    databaseFile = open(os.path.dirname(os.path.abspath(__file__)) + "\\scripts\\init-database.sql", "r")
+    for line in databaseFile:
+        commands += line
+    databaseFile.close()  
+    commands = (
+        f"""
+        {commands}
+        """
+        #is_active BOOLEAN DEFAULT TRUE <- Desnecessário
+    )
+    execute(commands)
