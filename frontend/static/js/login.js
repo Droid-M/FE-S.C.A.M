de03a1523cf750b9b30cf4e34caf51fd16d56d8a
danielLucas <daniel.lucasafj@gmail.com>
2021-05-28 16:53:28 -0300

    modificação do login

--- frontend/static/js/login.js
+++ frontend/static/js/login.js
@@ -1,85 +1,68 @@
-
-
-function logar(){
-    
-    function login(event) {
+async function logar(){  
         //stop submit the form, we will post it manually.
-        event.preventDefault();
+        // event.preventDefault();
     
         var username = document.getElementById("login").value;
         var password = document.getElementById("password").value;
-    
+        
+        console.log(username+' '+password);
+        
+       
+        
         var jsonData = {
-            username: username,
-            password: password,
+            CPF: username,
+            senha: password,
         };
-    
+
+    //    axios.get('/test').then((response)=>{
+    //        console.log(response.data);
+    //    })
+
+         axios.post('/auth',jsonData).then((response)=>{
+             console.log(response.data);
+         });
+
+        
         // disabled the submit button
-        $("#btnSubmit").prop("disabled", true);
-    
-        axios
-            // .post("/api/v" + VERSION + "/auth", jsonData)
-            // .then(function(response) {
-            //     console.log(response);
-            //     let access_token = response.data.access_token;
-            //     let user_class = response.data.user_class;
-            //     let first_login = response.data.first_login;
-            //     let username = response.data.cpf;
-            //     localStorage.setItem("access_token", access_token);
-            //     localStorage.setItem("user_class", user_class);
-            //     localStorage.setItem("first_login", first_login);
-            //     localStorage.setItem("username", username);
-            //     $("#btnSubmit").prop("disabled", false);
-            //     if (first_login === true) {
-            //         document.location = "/redefine";
-            //     } else if (user_class === "administrador") {
-            //         document.location = "/admin";
-            //     } else if (user_class === "agente") {
-            //         document.location = "/";
-            //     } else {
-            //         Swal.fire({
-            //             icon: "error",
-            //             title: "Erro desconhecido",
-            //             text: "Aconteceu algo inesperado, tente novamente. Caso o erro persista, entre em contato com o suporte.",
-            //             confirmButtonColor: "#2f3883"
-            //         });
-            //     }
+        // $("#btnSubmit").prop("disabled", true);
+        
+        //  axios
+        //     .post('/auth', jsonData)
+        //     .then(function(response) {
+
+                // let access_token = response.data.access_token;
+                // let user_class = response.data.user_class;
+                // let first_login = response.data.first_login;
+                // let username = response.data.cpf;
+                // console.log(response.data);
+                // localStorage.setItem("access_token", access_token);
+                // localStorage.setItem("user_class", user_class);
+                // localStorage.setItem("first_login", first_login);
+                // localStorage.setItem("username", username);
+                // $("#btnSubmit").prop("disabled", false);
+                // if(access_token){
+                //     console.log('Sucesso');
+                // }
+                // if (first_login === true) {
+                //     document.location = "/redefine";
+                // } else if (user_class === "administrador") {
+                //     document.location = "/admin";
+                // } else if (user_class === "agente") {
+                //     document.location = "/";
+                // } else {
+                //     Swal.fire({
+                //         icon: "error",
+                //         title: "Erro desconhecido",
+                //         text: "Aconteceu algo inesperado, tente novamente. Caso o erro persista, entre em contato com o suporte.",
+                //         confirmButtonColor: "#2f3883"
+                //     });
+                // }
             // })
-            .catch(function(error) {
-                if (error.response) {
-                    let msg = error.response.data.msg;
-                    $("#output").text(msg);
-                    if (typeof msg === "string" || msg instanceof String) {
-                        Swal.fire({
-                            icon: "error",
-                            title: "Credenciais erradas",
-                            text: msg,
-                            confirmButtonColor: "#2f3883"
-                        });
-                    } else {
-                        let completeMsg = '';
-                        for (let key in msg) {
-                            completeMsg += key + ': ' + msg[key] + '\n';
-                        }
-                        Swal.fire({
-                            icon: "error",
-                            title: "Erro",
-                            text: completeMsg,
-                            confirmButtonColor: "#2f3883"
-                        });
-                    }
-                } else {
-                    Swal.fire({
-                        icon: "error",
-                        title: "Erro desconhecido",
-                        text: "Aconteceu algo inesperado, tente novamente. Caso o erro persista, entre em contato com o suporte.",
-                        confirmButtonColor: "#2f3883"
-                    });
-                }
-            });
-        $("#btnSubmit").prop("disabled", false);
-    }
+            // .catch(function(error) {
+                
+            //     console.log(error.data);
+            // });
+        // $("#btnSubmit").prop("disabled", false);
+    
 }
-
-
 
