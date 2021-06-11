588bd049a98207415e530371d552698b55c58700
Tenshi Washenga <Suniotan@protonmail.com>
2021-06-11 15:02:33 -0300

    Cria um alerta de erro

--- frontend/static/js/login.js
+++ frontend/static/js/login.js
@@ -1,92 +1,89 @@
-function logar(){  
-        //stop submit the form, we will post it manually.
-        // event.preventDefault();
-    
-        var username = document.getElementById("login").value;
-        var password = document.getElementById("password").value;
-        
-        console.log('Usuário e Senha:',username+' '+password);
-        
-       
-        
-        var jsonData = {
-            CPF: username,
-            senha: password,
-        };
+function logar() {
+  //stop submit the form, we will post it manually.
+  // event.preventDefault();
 
-        // Faz uma requisição post para a api, esperando como resultado o token 
-         axios.post('/auth',jsonData).then((response)=>{
-            console.log(response.data); // json com os dados
-            let status = response.data.status;
+  var username = document.getElementById("login").value;
+  var password = document.getElementById("password").value;
 
-            if(status == '201'){
+  console.log("Usuário e Senha:", username + " " + password);
 
-                let access_token = response.data.access_token;
-                let classe = response.data.classe;
-   
-                sessionStorage.setItem('access_token',access_token);
+  var jsonData = {
+    CPF: username,
+    senha: password,
+  };
 
-                if(classe == "ADMINISTRADOR"){
-                    location.href('/admin');
-                }else if(classe == "ENFERMEIRO_CHEFE"){
-                    location.href("/enf-chefe");
-                }else if(classe == "ENFERMEIRO"){
-                    location.href("/enf");
-                }else if(classe == "ESTAGIARIO"){
-                    location.href("/estagiario");
-                }
-            }else if(status == '406'){
-                let msg = response.data.msg;
-                alert(msg);
-                location.href("/");
-            }
+  // Faz uma requisição post para a api, esperando como resultado o token
+  axios
+    .post("/auth", jsonData)
+    .then((response) => {
+      console.log(response.data); // json com os dados
+      let status = response.data.status;
 
-             
-         }).catch((error)=>{
-             console.log(error);
-         });
+      if (status == "201") {
+        let access_token = response.data.access_token;
+        let classe = response.data.classe;
 
-        
-        // disabled the submit button
-        // $("#btnSubmit").prop("disabled", true);
-        
-        //  axios
-        //     .post('/auth', jsonData)
-        //     .then(function(response) {
+        sessionStorage.setItem("access_token", access_token);
 
-                // let access_token = response.data.access_token;
-                // let user_class = response.data.user_class;
-                // let first_login = response.data.first_login;
-                // let username = response.data.cpf;
-                // console.log(response.data);
-                // localStorage.setItem("access_token", access_token);
-                // localStorage.setItem("user_class", user_class);
-                // localStorage.setItem("first_login", first_login);
-                // localStorage.setItem("username", username);
-                // $("#btnSubmit").prop("disabled", false);
-                // if(access_token){
-                //     console.log('Sucesso');
-                // }
-                // if (first_login === true) {
-                //     document.location = "/redefine";
-                // } else if (user_class === "administrador") {
-                //     document.location = "/admin";
-                // } else if (user_class === "agente") {
-                //     document.location = "/";
-                // } else {
-                //     Swal.fire({
-                //         icon: "error",
-                //         title: "Erro desconhecido",
-                //         text: "Aconteceu algo inesperado, tente novamente. Caso o erro persista, entre em contato com o suporte.",
-                //         confirmButtonColor: "#2f3883"
-                //     });
-                // }
-            // })
-            // .catch(function(error) {
-                
-            //     console.log(error.data);
-            // });
-        // $("#btnSubmit").prop("disabled", false);
-    
+        if (classe == "ADMINISTRADOR") {
+          location.href("/admin");
+        } else if (classe == "ENFERMEIRO_CHEFE") {
+          location.href("/enf-chefe");
+        } else if (classe == "ENFERMEIRO") {
+          location.href("/enf");
+        } else if (classe == "ESTAGIARIO") {
+          location.href("/estagiario");
+        }
+      }
+    })
+    .catch((error) => {
+      let msg = error.response.data.msg;
+      console.error(msg);
+      $("#alert_placeholder").html(
+        '<div class="alert alert-danger alert-dismissible">' +
+          msg +
+          "</div>"
+      );
+    });
+
+  // disabled the submit button
+  // $("#btnSubmit").prop("disabled", true);
+
+  //  axios
+  //     .post('/auth', jsonData)
+  //     .then(function(response) {
+
+  // let access_token = response.data.access_token;
+  // let user_class = response.data.user_class;
+  // let first_login = response.data.first_login;
+  // let username = response.data.cpf;
+  // console.log(response.data);
+  // localStorage.setItem("access_token", access_token);
+  // localStorage.setItem("user_class", user_class);
+  // localStorage.setItem("first_login", first_login);
+  // localStorage.setItem("username", username);
+  // $("#btnSubmit").prop("disabled", false);
+  // if(access_token){
+  //     console.log('Sucesso');
+  // }
+  // if (first_login === true) {
+  //     document.location = "/redefine";
+  // } else if (user_class === "administrador") {
+  //     document.location = "/admin";
+  // } else if (user_class === "agente") {
+  //     document.location = "/";
+  // } else {
+  //     Swal.fire({
+  //         icon: "error",
+  //         title: "Erro desconhecido",
+  //         text: "Aconteceu algo inesperado, tente novamente. Caso o erro persista, entre em contato com o suporte.",
+  //         confirmButtonColor: "#2f3883"
+  //     });
+  // }
+  // })
+  // .catch(function(error) {
+
+  //     console.log(error.data);
+  // });
+  // $("#btnSubmit").prop("disabled", false);
 }
-
