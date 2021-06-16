15fd8b3663832d476f327ff993490dd1728b0fea
danielLucas <daniel.lucasafj@gmail.com>
2021-06-16 11:02:50 -0300

    Listagem de usuarios em tabela funcionando

--- frontend/static/js/adm.controller.js
+++ frontend/static/js/adm.controller.js
@@ -5,25 +5,24 @@ const access_token = sessionStorage.getItem("access_token");
 /**
  * Edita e Atualiza usuario 
  */
-function update_user(usuario){
-    console.log('hello')
-
-    
+function update_user(id_user){
+    console.log(id_user)
+   
 }
 
 
 /**
  * Deleta Usuarios
  */
-function delete_user(){
-
+function delete_user(id_user){
+    alert("Deletar usuario " +id_user);
 }
 
 /**
  * Busca todos os usuários cadastrados
  */
 function get_all_users(){
-   
+
 
     axios.get("/lista_usuario",{ 
         headers: { Authorization: `Bearer ${access_token}` }
@@ -42,80 +41,34 @@ function get_all_users(){
  * @param {*} usuarios 
  */
 function cria_tabela(usuarios){
-    var table = document.getElementById('tb_user');
     var tabela = document.getElementById('tBody');
-    
-    var newRow = tabela.insertRow(table.length);
 
-    var cell_cpf = newRow.insertCell(0);
-    var cell_nome = newRow.insertCell(1);
-    var cell_funcao = newRow.insertCell(2);
-    var cell_edite = newRow.insertCell(3);
-    var cell_delete = newRow.insertCell(4);
-    const edit = `<a onclick=${update_user(usuarios[0])}>Editar</a>`
-    const del = `<a onclick=${update_user(usuarios[0])}>Delete</a>`
-    cell_cpf.innerHTML=usuarios[0].CPF;
-    cell_nome.innerHTML = usuarios[0].nome;
-    cell_funcao.innerHTML=usuarios[0].tipo;
-    
-    cell_edite.innerHTML=edit;
-    cell_delete.innerHTML=del;
+    for(let i=0;i<usuarios.length;i++){
 
-    // var link_deleta = document.createElement("a"); 
-    // link_deleta.text="Deleta"
+        var newRow = tabela.insertRow();
     
-    // for(let i=0; i<usuarios.length;i++){
-
-    //     //criar elementos
-    //     var linha = document.createElement("tr");
-    //     var campo_cpf = document.createElement("td");
-    //     var campo_nome = document.createElement("td");
-    //     var campo_funcao = document.createElement("td");
-    //     var campo_edite = document.createElement("td");
-    //     var campo_delete = document.createElement("td");
+        var cell_cpf = newRow.insertCell();
+        var cell_nome = newRow.insertCell();
+        var cell_funcao = newRow.insertCell();
+        let cell_editar = newRow.insertCell();
+        let cell_deletar = newRow.insertCell();
+    
+    
+        let btEditar = document.createElement('button');
+        btEditar.innerHTML='Editar';
+        btEditar.setAttribute('onclick',`update_user(${usuarios[i].CPF})`);
+    
+        let btDeletar = document.createElement('button');
+        btDeletar.innerHTML='Deletar';
+        btDeletar.setAttribute('onclick',`delete_user(${usuarios[i].CPF})`);
         
-    //     var link_edita = document.createElement("a");
-    //     var link_deleta = document.createElement("a"); 
         
-    //     var btn_edita = document.createElement("button");
-        
-    //     //cria os nós
-    //     var texto_cpf = document.createTextNode(usuarios[i].CPF);
-    //     var texto_nome = document.createTextNode(usuarios[i].nome);
-    //     var texto_funcao = document.createTextNode(usuarios[i].tipo);
-       
-    //     link_edita.setAttribute('edita',"edita");
-    //     link_edita.text='Edita'
-       
-    //     btn_edita.textContent='Edita';
-        
-    //     btn_edita.
-
-    //     link_deleta.setAttribute('delete','delete');
-    //     link_deleta.href='#';
-    //     link_deleta.text='Delete'
-
-    //     //vincular os nós aos elementos
-    //     campo_cpf.appendChild(texto_cpf);
-    //     campo_nome.appendChild(texto_nome);
-    //     campo_funcao.appendChild(texto_funcao);
-    //     campo_edite.appendChild(btn_edita);
-    //     campo_delete.appendChild(link_deleta);
-    
-    //     linha.appendChild(campo_cpf);
-    //     linha.appendChild(campo_nome);
-    //     linha.appendChild(campo_funcao);
-    //     linha.appendChild(campo_edite);
-    //     linha.appendChild(campo_delete);
-
-       
-    
-    //     //vincula os elementos ao documento
-    //     tabela.appendChild(linha);
-
-
-     
-    // }
+        cell_cpf.innerHTML=usuarios[i].CPF;
+        cell_nome.innerHTML = usuarios[i].nome;
+        cell_funcao.innerHTML=usuarios[i].tipo;
+        cell_editar.appendChild(btEditar);
+        cell_deletar.appendChild(btDeletar);
+    }
 
 }
 
