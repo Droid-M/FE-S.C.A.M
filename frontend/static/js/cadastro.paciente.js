5083d03ef04b73d5ed2cf420138cf43aa633f95a
danielLucas <daniel.lucasafj@gmail.com>
2021-06-20 17:13:38 -0300

    Implementado Cadastro, Exclusao de paciente

---
+++ frontend/static/js/cadastro.paciente.js
@@ -0,0 +1,139 @@
+const access_token = sessionStorage.getItem("access_token");
+
+const url = location.href;
+
+let state = false;
+let params = url.split("=");
+let id='';
+
+if(params.length>1){
+    id = params[1];
+    state=true;
+}
+
+if(state && id!=undefined){
+    update_paci();
+    let btn = document.getElementById('btnCadastrar');
+    btn.innerHTML="Salvar";
+}
+
+
+/**
+ * Cadastra um novo paciente no sistema
+ */
+ function cadastro_paciente(){
+
+    let nome = document.getElementById('nome').value;
+    let sobrenome = document.getElementById('sobrenome').value;
+    let genero = document.getElementById('genero').value;
+    let sexo = document.getElementById('sexo').value;
+    let cpf = document.getElementById('cpf').value;
+    let dataNascimento = document.getElementById('dataNascimento').value;
+    let select = document.getElementById('sangue');
+    let tipoSangue = select.options[select.selectedIndex].value;
+
+    let endereco = document.getElementById('endereco').value;
+    let telefone = document.getElementById('telefone').value;
+
+    let diagnostico = document.getElementById('dados').value;
+    let id_func = document.getElementById('responsavel').value;
+
+    let dataFormatada='';
+
+    //verifica se a data nao é vazia e faz a alteração para salvar no BD
+    if(dataNascimento.length>0){
+
+        let data_spli = dataNascimento.split("/");
+        let date = new Date(data_spli[2],data_spli[1]-1,data_spli[0]);
+        dataFormatada =(date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate());
+
+    }
+
+    // verifica se o cpf nao está vazio e faz a alteração para salvar no BD
+    if(cpf.length>0){
+        let result_cpf = cpf.split(".")
+        cpf='';
+        result_cpf.forEach(numbers => {
+            cpf+=numbers;
+        });
+       
+        let result2_cpf = cpf.split("-")
+        cpf='';
+        result2_cpf.forEach((n)=>{
+            cpf+=n;
+        })
+    }
+   
+
+    let paciente = {
+        CPF: cpf,
+        nome:nome+" "+ sobrenome,
+        sexo: sexo,
+        genero: genero,
+        data_nascimento:dataFormatada,
+        tipo_sangue: tipoSangue,
+        endereco: endereco,
+        telefone: telefone,
+        atendente_id: id_func,
+        dados: diagnostico
+    }
+    axios({
+        method: state ?'put':'post',
+        url: state ? `/paciente/${id}` : '/paciente',
+        headers: { Authorization: `Bearer ${access_token}` },
+        data: paciente
+           
+        
+    }). then((response)=>{
+            let nome = response.data.nome;
+            let CPF = response.data.CPF;
+
+            if(state){
+                alert(`Paciente ${nome} Atualizado com Sucesso`)
+            }else{
+                alert(`Paciente  ${nome} Cadastrado com Sucesso` );
+            }
+            location.href='/enf-chefe'
+        
+        console.log(response.data); 
+    }).catch((error)=>{
+        console.error(error);
+    })
+    
+    console.log(paciente);
+ 
+
+  
+}
+
+/**
+ * Carrega os dados do paciente para ser editado
+ */
+function update_paci(){
+    let paci ='';
+
+        axios.get(`/paciente/${id}`,{
+            headers: { Authorization: `Bearer ${access_token}` }
+        }).then((response)=>{
+            paci = response.data;
+
+            let nome = document.getElementById('nome').value = paci.nome;
+            // let sobrenome = document.getElementById('sobrenome').value;
+            let genero = document.getElementById('genero').value = paci.genero;
+            let sexo = document.getElementById('sexo').value = paci.sexo;
+            let cpf = document.getElementById('cpf').value=paci.CPF;
+            let dataNascimento = document.getElementById('dataNascimento').value = formata_data(paci.data_nascimento);
+            let select = document.getElementById('sangue');
+            let tipoSangue = select.options[select.selectedIndex].value;
+        
+            let endereco = document.getElementById('endereco').value = paci.endereco;
+            let telefone = document.getElementById('telefone').value = paci.telefone;
+        
+            let diagnostico = document.getElementById('dados').value = paci.dados;
+            let id_func = document.getElementById('responsavel').value = paci.atendente_id;
+        }).catch((error)=>{
+            console.error(error);
+        })
+    
+    
+}
--- frontend/static/js/enfChefe.controller.js
+++ frontend/static/js/enfChefe.controller.js
@@ -1,85 +1,105 @@
 
 const token = sessionStorage.getItem("access_token");
+let pacientes={};
+get_all_pacientes();
 
-function cadastro_paciente(){
-
-    let nome = document.getElementById('nome').value;
-    let sobrenome = document.getElementById('sobrenome').value;
-    let genero = document.getElementById('genero').value;
-    let sexo = document.getElementById('sexo').value;
-    let cpf = document.getElementById('cpf').value;
-    let dataNascimento = document.getElementById('dataNascimento').value;
-    let select = document.getElementById('sangue');
-    let tipoSangue = select.options[select.selectedIndex].value;
-
-    let endereco = document.getElementById('endereco').value;
-    let telefone = document.getElementById('telefone').value;
-
-    let diagnostico = document.getElementById('dados').value;
-    let id_func = document.getElementById('responsavel').value;
-
-    let dataFormatada='';
-
-    if(dataNascimento.length>0){
-
-        let data_spli = dataNascimento.split("/");
-        let date = new Date(data_spli[2],data_spli[1]-1,data_spli[0]);
-        dataFormatada =(date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate());
-
+/**
+ * Atualiza os dados de um paciente
+ */
+function update_paciente(id_paci){
+    let paci = '';
+    if(pacientes.length>0){
+        paci = pacientes[id_paci];
     }
+    location.href=`/cadastrar-paciente/id=${paci.CPF}`;
 
+}
 
-    if(cpf.length>0){
-        let result_cpf = cpf.split(".")
-        cpf='';
-        result_cpf.forEach(numbers => {
-            cpf+=numbers;
-        });
-       
-        let result2_cpf = cpf.split("-")
-        cpf='';
-        result2_cpf.forEach((n)=>{
-            cpf+=n;
+/**
+ * Exclui um paciente do sistema da uti
+ * @param {*} index 
+ */
+function delete_paciente(index){
+    if(pacientes.length>0){
+        let paci = pacientes[index];
+
+        axios.delete(`/paciente/${paci.CPF}`,{
+            headers: { Authorization: `Bearer ${token}` }
+        }).then((response)=>{
+            alert("Usuario Deletado com Sucesso")
+            location.href='/enf-chefe';
+    
+        }).catch((error)=>{
+            console.error(error)
         })
     }
-   
+}
 
-    let paciente = {
-        CPF: cpf,
-        nome:nome+" "+ sobrenome,
-        sexo: sexo,
-        genero: genero,
-        data_nascimento:dataFormatada,
-        tipo_sangue: tipoSangue,
-        endereco: endereco,
-        telefone: telefone,
-        atendente_id: id_func,
-        dados: diagnostico
-    }
-    axios({
-        method: 'post',
-        url:'/paciente',
-        headers: { Authorization: `Bearer ${token}` },
-        data: paciente
-           
-        
-    }). then((response)=>{
-            let nome = response.data.nome;
-            let CPF = response.data.CPF;
+/**
+ * Busca todos os pacientes cadastrados
+ */
+function get_all_pacientes(){
 
+    axios.get('/paciente',{
+        headers: { Authorization: `Bearer ${token}` }
+    }).then((response)=>{
+        pacientes = response.data;
 
-            alert(`Paciente Cadastrado com Sucesso: ${nome} ${CPF}` );
-            location.href='/enf-chefe'
-        
-        console.log(response.data); 
+        cria_tabela_paci(pacientes);
+      
     }).catch((error)=>{
         console.error(error);
     })
-    
-    console.log(paciente);
- 
-
-  
 }
 
+/**
+ * Cria uma tabela que lista todos os pacientes cadastrados na UTI
+ * @param {*} pacientes Lista de todos os pacientes cadastrados
+ */
+function cria_tabela_paci(pacientes){
+    const tabela = document.getElementById('tBody');
 
+    for(let i=0;i<pacientes.length;i++){
+
+        var newRow = tabela.insertRow();
+
+        var cell_cpf = newRow.insertCell();
+        var cell_nome = newRow.insertCell();
+        var cell_dataNascimento = newRow.insertCell();
+        var cell_dataInternacao = newRow.insertCell();
+        var cell_prontuario = newRow.insertCell();
+        var cell_editar = newRow.insertCell();
+        var cell_delete= newRow.insertCell();
+
+        let btEditar = document.createElement('button');
+        btEditar.innerHTML='Editar';
+        btEditar.setAttribute('onclick',`update_paciente(${i})`);
+        btEditar.setAttribute('id','btnEditar');
+
+        let btDeletar = document.createElement('button');
+        btDeletar.innerHTML='Deletar';
+        btDeletar.setAttribute('onclick', `delete_paciente(${i})`);
+        btDeletar.setAttribute('id','btnDeletar');
+
+
+        cell_cpf.innerHTML = pacientes[i].CPF;
+        cell_nome.innerHTML = pacientes[i].nome;
+        cell_dataNascimento.innerHTML =formata_data(pacientes[i].data_nascimento);
+        cell_dataInternacao.innerHTML = formata_data(pacientes[i].created_on);
+        cell_prontuario.innerHTML= pacientes[i].dados;
+        cell_editar.appendChild(btEditar);
+        cell_delete.appendChild(btDeletar);
+
+    }
+}
+
+/**
+ * Formata Data para o formato DD-MM-YYYY
+ * @param {*} dados String data
+ * @returns string - Data Formatada
+ */
+function formata_data(dados){
+    var data = new Date(dados);
+    var dataFormatada = (data.getDate()+"/"+data.getMonth()+"/"+data.getFullYear());
+    return dataFormatada;
+}
